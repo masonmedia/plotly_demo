7f31ea0b2162f514e7546862dd90e2fa34003bb3
Andrew Mason <andrewmasonmedia@gmail.com>
2022-09-15 11:57:22 -0400

    Initial commit

---
+++ components/LogosComponent.js
@@ -0,0 +1,31 @@
+export default function LogosComponent() {
+    return (
+        // <div>
+            <div className="col-lg-6 d-flex flex-column justify-content-end align-items-center pt-4 pt-lg-5 pb-3">
+                <div className="row w-100">
+                  <div className="col d-flex justify-content-center align-items-center px-2 py-4">
+                    <img src="/img/logos/colgate.png" alt=""/>
+                  </div>
+                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                    <img src="/img/logos/nvidia.png" alt=""/>
+                  </div>
+                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                    <img src="/img/logos/kc.png" alt="" width="75px"/>
+                  </div>
+                </div>
+
+                <div className="row w-100">
+                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                    <img src="/img/logos/jazz.png" alt=""/>
+                  </div>
+                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                    <img src="/img/logos/tmobile.png" alt=""/>
+                  </div>
+                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                    <img src="/img/logos/tva.png" alt="" width="75px"/>
+                  </div>
+                </div>
+            </div>
+        // </div>
+    )
+}
