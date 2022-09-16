cc9e82d876c815f921a909d6defe37aa10a3512a
Andrew Mason <andrewmasonmedia@gmail.com>
2022-09-16 15:38:27 -0400

    Added fade animation on load, moved text section to component, removed unused code

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { useState } from 'react';
 import FormComponent from '../components/FormComponent'
 import LogosComponent from '../components/LogosComponent'
 import NavComponent from '../components/NavComponent'
-import ImageComponent from '../components/ImageComponent';
+import ImageComponent from '../components/ImageComponent'
+import TextComponent from '../components/TextComponent';
 
 export default function Home() {
   const [theme, setTheme] = useState('light');
@@ -13,7 +14,7 @@ export default function Home() {
     setTheme(newTheme);
   }
   return (
-    <div>
+    <div className="fade-in">
       <Head>
         <title>Plotly Landing Page</title>
         <meta name="description" content="See Dash in action! Sign up for your free demo today." />
@@ -42,21 +43,10 @@ export default function Home() {
        
         <div className="container-fluid">
           <div className="row min-vh-100 p-5">
-            <div className="col-lg-6 d-flex flex-column justify-content-start align-items-start pe-5">
-              <h1 className="mb-0">See Dash in action</h1>
-              <h5 className="sub-heading pb-3 pt-2 pe-3">Thank you for your interest in Dash Enterprise. We'll be in touch soon to schedule a demo.</h5>
-              {/* list */}
-              <ul className="list pe-0 pe-lg-4">
-                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
-                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
-                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
-              </ul>
-            </div>
-
+            <TextComponent />
             {/* ghost spacer for desktop */}
             <div className="col-lg-6 d-none d-lg-flex flex-column justify-content-start align-items-start p-5 my-4 my-lg-0">
             </div>
-
             <FormComponent formClass={ theme === 'light' ? 'bg-white border-light' : 'bg-dark border-dark'} />
             <ImageComponent />
             <LogosComponent />
