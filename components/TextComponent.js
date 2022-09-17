bfc76e5271f2abc2560bfc78d621d9f45913c245
Andrew Mason <andrewmasonmedia@gmail.com>
2022-09-17 14:13:17 -0400

    Mobile styling additions

--- components/TextComponent.js
+++ components/TextComponent.js
@@ -1,7 +1,7 @@
 export default function TextComponent() {
     return (
         <div className="col-lg-6 d-flex flex-column justify-content-start align-items-start px-0 pe-md-5">
-            <h1 className="mb-0">See Dash in action</h1>
+            <h1 className="mb-2 lh-sm">See Dash in action</h1>
             <h5 className="sub-heading pb-3 pt-2 pe-3">Thank you for your interest in Dash Enterprise. We'll be in touch soon to schedule a demo.</h5>
             {/* list */}
             <ul className="list pe-0 pe-lg-4">
