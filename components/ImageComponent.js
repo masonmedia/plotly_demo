b8e68cadccbd7d50437ffee1ef8cf302c19d4ae5
Andrew Mason <andrewmasonmedia@gmail.com>
2022-09-17 15:07:15 -0400

    Update

--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -11,7 +11,6 @@ export default function ImageComponent() {
 
             {/* image mobile */}
             <div className="col-lg-6 d-flex d-lg-none flex-column justify-content-center align-items-center px-0 px-md-2">
-              {/* <img className="w-100 rounded position-relative" style={{ marginTop: '-60px', zIndex: '0' }} src="/img/image.png" /> */}
                 <div style={{ marginTop: '-60px', zIndex: '0' }}>
                     <Image src={image} alt="Plotly Dash Club 2022" />
                 </div> 
