2dc7318f004d78c1cf8706544b44e796b256e7ac
Andrew Mason <andrewmasonmedia@gmail.com>
2022-09-16 20:59:36 -0400

    Trial escape apostrophe TextComponent

--- components/TextComponent.js
+++ components/TextComponent.js
@@ -2,12 +2,12 @@ export default function TextComponent() {
     return (
         <div className="col-lg-6 d-flex flex-column justify-content-start align-items-start pe-5">
             <h1 className="mb-0">See Dash in action</h1>
-            <h5 className="sub-heading pb-3 pt-2 pe-3">Thank you for your interest in Dash Enterprise. We'll be in touch soon to schedule a demo.</h5>
+            <h5 className="sub-heading pb-3 pt-2 pe-3">Thank you for your interest in Dash Enterprise. We\'ll be in touch soon to schedule a demo.</h5>
             {/* list */}
             <ul className="list pe-0 pe-lg-4">
-                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
-                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
-                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
             </ul>
         </div>
     )
