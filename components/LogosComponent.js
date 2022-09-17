0ccf41d782d2303724cd0d68a74947da6fe88e85
Andrew Mason <andrewmasonmedia@gmail.com>
2022-09-16 20:47:42 -0400

    Update to LogosComponent Image

--- components/LogosComponent.js
+++ components/LogosComponent.js
@@ -8,38 +8,30 @@ import tva from '../public/img/logos/tva.png'
 
 export default function LogosComponent() {
     return (
-        // <div>
-            <div className="col-lg-6 d-flex flex-column justify-content-end align-items-center pt-4 pt-lg-5 pb-3">
-                <div className="row w-100">
-                  <div className="col d-flex justify-content-center align-items-center px-2 py-4">
-                    {/* <img src="/img/logos/colgate.png" alt=""/> */}
-                    <Image src={colgate} alt="Colgate logo" />
-                  </div>
-                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
-                    {/* <img src="/img/logos/nvidia.png" alt=""/> */}
-                    <Image src={nvidia} alt="nvidia logo" />
-                  </div>
-                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
-                    {/* <img src="/img/logos/kc.png" alt="" width="75px"/> */}
-                    <Image src={kc} width='75px' alt="Kansas City Chiefs logo" />
-                  </div>
+          <div className="col-lg-6 d-flex flex-column justify-content-end align-items-center pt-4 pt-lg-5 pb-3">
+              <div className="row w-100">
+                <div className="col d-flex justify-content-center align-items-center px-2 py-4">
+                  <Image src={colgate} alt="Colgate logo" />
                 </div>
+                <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                  <Image src={nvidia} alt="nvidia logo" />
+                </div>
+                <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                  <Image src={kc} width='75px' alt="Kansas City Chiefs logo" />
+                </div>
+              </div>
 
-                <div className="row w-100">
-                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
-                    <img src="/img/logos/jazz.png" alt=""/>
-                    <Image src={jazz} alt="Utah Jazz logo" />
-                  </div>
-                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
-                    {/* <img src="/img/logos/tmobile.png" alt=""/> */}
-                    <Image src={tmobile} alt="tmobile logo" />
-                  </div>
-                  <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
-                    {/* <img src="/img/logos/tva.png" alt="" width="75px"/> */}
-                    <Image src={tva} width='75px' alt="TVA logo" />
-                  </div>
+              <div className="row w-100">
+                <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                  <Image src={jazz} alt="Utah Jazz logo" />
                 </div>
-            </div>
-        // </div>
+                <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                  <Image src={tmobile} alt="tmobile logo" />
+                </div>
+                <div className="col d-flex d-flex justify-content-center align-items-center px20 py-4">
+                  <Image src={tva} width='75px' alt="TVA logo" />
+                </div>
+              </div>
+          </div>
     )
 }
--- components/TextComponent.js
+++ components/TextComponent.js
@@ -5,9 +5,9 @@ export default function TextComponent() {
             <h5 className="sub-heading pb-3 pt-2 pe-3">Thank you for your interest in Dash Enterprise. We'll be in touch soon to schedule a demo.</h5>
             {/* list */}
             <ul className="list pe-0 pe-lg-4">
-            <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
-            <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
-            <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
+                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
+                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
+                <li>Culpa ullamco ipsum consequat nisi qui non veniam quis deserunt sit aute cillum ullamco.</li>
             </ul>
         </div>
     )
